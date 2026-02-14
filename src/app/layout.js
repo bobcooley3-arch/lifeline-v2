a9019b0cb338244ee59aade9150e6a6a30d73eb3
bobcooley3-arch <bob.cooley3@googlemail.com>
2026-02-14 08:54:21 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import './globals.css'
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#0f172a' }}>
+        {children}
+      </body>
     </html>
   )
 }
